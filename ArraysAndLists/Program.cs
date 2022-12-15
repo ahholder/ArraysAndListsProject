ee20ac0f71d3c90219f842bcf29e821249f23731
Alexander Holder <drowish95z@gmail.com>
2022-12-15 10:26:52 -0600

    Complete Assignment

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ArraysAndLists
 {
@@ -9,11 +11,18 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            var Array = new int[10];
+            for (var i = 0; i < 10; i++)
+            {
+                Array[i] = i + 1;
+            }
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+            var evens = new List<int>();
+            var odds = new List<int>();
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -22,11 +31,46 @@ namespace ArraysAndLists
              * or the odds List
              */
 
+            for (var i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] % 2 == 0)
+                {
+                    evens.Add(Array[i]);
+                } else
+                {
+                    odds.Add(Array[i]);
+                }
+            }
+
             /* Now using foreach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+            string result = "Evens: ";
+            bool spacer = false;
+            foreach (var item in evens)
+            {
+                if (spacer == true)
+                {
+                    result += ", ";
+                }
+                result += item;
+                spacer = true;
+            }
+            Console.WriteLine(result);
+            spacer = false;
+            result = "Odds: ";
+            foreach (var item in odds)
+            {
+                if (spacer == true)
+                {
+                    result += ", ";
+                }
+                result += item;
+                spacer = true;
+            }
+            Console.WriteLine(result);
         }
     }
 }
